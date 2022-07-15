0852fcb246a5f8ab3748869057bcf93fcb552c7d
Rango <34990962+the-rango@users.noreply.github.com>
2022-07-15 16:50:29 -0700

    Init w/ Prev Changes

---
+++ src/App.js
@@ -0,0 +1,125 @@
+import {
+  Box,
+  Tabs,
+  Tab,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import * as React from "react";
+import {
+  BarChart,
+  // Celebration,
+  Input,
+  DarkMode,
+  LightMode,
+} from "@mui/icons-material";
+import Ticketing from "./Ticketing";
+import Stats from "./Stats";
+// import Drawing from "./Drawing";
+
+import carrierDark from "./carrier-darkm.png"
+import carrierLight from "./carrier-lightm.png"
+
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme =  createTheme({
+  palette: {
+    mode: 'dark',
+    // primary: {
+    //   main: '#1452ee',
+    // },
+    primary: {
+      main: '#eeb114',
+    },
+  }
+});
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    // primary: {
+    //   main: '#1452ee',
+    // },
+    primary: {
+      main: '#eeb114',
+    },
+  }
+});
+
+function App() {
+  const [tab, setTab] = React.useState(0);
+  const [dark, setDark] = React.useState(useMediaQuery('(prefers-color-scheme: dark)'));
+
+  const [scrWidth, setScrWidth] = React.useState(window.innerWidth);
+  const updateWidth = () => {
+    console.log(window.innerWidth);
+    setScrWidth(typeof window !== "undefined" ? window.innerWidth : 0);
+  }
+
+  React.useEffect(()=>{
+    window.addEventListener("resize", updateWidth)
+    return () => {
+      window.removeEventListener("resize", updateWidth)
+    }
+  })
+
+  return (
+    <ThemeProvider theme={dark ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <React.Fragment className="App">
+        <Box
+          sx={{
+            flexGrow: 1,
+            bgcolor: "background.paper",
+            height: "100%"
+          }}
+        >
+        <AppBar position="static">
+        <Toolbar variant="dense">
+          <img src={dark ? carrierDark : carrierLight} alt="jt-carrier-logo"/>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginLeft: 3 }}>
+            JT Carrier
+          </Typography>
+
+
+          <Tabs
+            value={tab}
+            onChange={(e,newMode)=>setTab(newMode)}
+            textColor="secondary"
+            indicatorColor="secondary"
+            sx={{
+                  bottom: "0px",
+                }}
+          >
+            <Tab icon={<Input />} iconPosition="start" label="Submit" />
+            <Tab
+              icon={<BarChart />}
+              iconPosition="start"
+              label="Stats"
+            />
+            {/*<Tab
+              icon={<Celebration />}
+              iconPosition="start"
+              label="WINGS Drawing"
+            />*/}
+          </Tabs>
+
+          <IconButton color="inherit" onClick={()=>setDark(!dark)}>
+            {dark ? <LightMode /> : <DarkMode />}
+          </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Box sx={{width: scrWidth > 750 ? "50%" : "90%", margin: "auto"}}>
+          {tab === 0 ? <Ticketing /> : <Stats />}
+        </Box>
+        </Box>
+      </React.Fragment>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/Stats.js
@@ -0,0 +1,153 @@
+import {
+  Box,
+  Typography,
+  Table,
+  TableContainer,
+  Paper,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableBody
+} from "@mui/material";
+import * as React from "react";
+
+function Stats() {
+  const [letters, setLetters] = React.useState({
+    w: 0,
+    i: 0,
+    n: 0,
+    g: 0,
+    s: 0
+  });
+  const [top, setTop] = React.useState("");
+  const [errmsg, setErrmsg] = React.useState("");
+
+  const updateStats = () => {
+    try {
+      fetch("https://wings-971b6-default-rtdb.firebaseio.com/letters.json")
+        .then((response) => response.json())
+        .then((data) => setLetters(data));
+
+      fetch("https://wings-971b6-default-rtdb.firebaseio.com/top.json")
+        .then((response) => response.json())
+        .then((data) => setTop(data));
+
+    } catch (err) {
+      setErrmsg("Something went wrong...our server might be busy!");
+    }
+  };
+
+  updateStats();
+  setInterval(updateStats, 100000);
+
+  return (
+    <Box sx={{
+      display: "flex",
+      flexDirection: "column"
+    }}>
+      <Typography variant="h5" sx={rowStyle}>WINGS Ticket Stats</Typography>
+      <Typography variant="h6">for the week of May 02-06</Typography>
+      <Typography variant="h6">{errmsg}</Typography>
+
+      <TableContainer component={Paper} sx={rowStyle}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell
+                sx={{  fontWeight: "bold" }}
+                align="center"
+              >
+                W
+              </TableCell>
+              <TableCell
+                sx={{  fontWeight: "bold" }}
+                align="center"
+              >
+                I
+              </TableCell>
+              <TableCell
+                sx={{  fontWeight: "bold" }}
+                align="center"
+              >
+                N
+              </TableCell>
+              <TableCell
+                sx={{  fontWeight: "bold" }}
+                align="center"
+              >
+                G
+              </TableCell>
+              <TableCell
+                sx={{  fontWeight: "bold" }}
+                align="center"
+              >
+                S
+              </TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            <TableRow>
+              <TableCell sx={{ }} align="center">
+                {letters["w"]}
+              </TableCell>
+              <TableCell sx={{ }} align="center">
+                {letters["i"]}
+              </TableCell>
+              <TableCell sx={{ }} align="center">
+                {letters["n"]}
+              </TableCell>
+              <TableCell sx={{ }} align="center">
+                {letters["g"]}
+              </TableCell>
+              <TableCell sx={{ }} align="center">
+                {letters["s"]}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Typography variant="h6" sx={rowStyle}>
+        This trimester's cutoff for TOP 25%: {top} tickets
+      </Typography>
+
+      {/*
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 350, marginTop: 5 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }} align="left">
+                Teacher
+              </TableCell>
+              <TableCell sx={{ fontWeight: "bold" }} align="left">
+                Count
+              </TableCell>
+            </TableRow>
+          </TableHead>
+
+
+          <TableBody>
+            {staff.map((member) => (
+              <TableRow>
+                <TableCell>{member.label}</TableCell>
+                <TableCell>{Math.floor(Math.random() * 30) + 20}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+        </Table>
+      </TableContainer>
+      */}
+    </Box>
+  );
+}
+
+const rowStyle = {
+  width: "100%",
+  flexGrow: "row",
+  display: "flex",
+  marginTop: 3,
+};
+
+export default Stats;
